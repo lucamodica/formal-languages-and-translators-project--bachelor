2f0ba4742791ec5645a21bb71556c5dfa0b0e58c
Luca Modica <luca.modica55@gmail.com>
2020-10-16 18:00:58 +0200

    Es1_3, done

---
+++ lab/L1 (DFA implementation)/Es1_3.java
@@ -0,0 +1,75 @@
+public class Es1_3 extends Automata{
+
+    public boolean scan(String s){
+        //int state = 0;
+        int i = 0;
+
+        //Analyzing the input string
+        while(getState() >= 0 && i < s.length()){
+            //Current char to be analyzed
+            final char ch = s.charAt(i++);
+
+            //State printing (only for debugging)
+            //System.out.println("State rn: " + getState());
+
+            switch (getState()) {
+                case 0:
+                    if ((int) ch % 2 == 0){ setState(1); }
+                    else if ((int) ch % 2 == 1){ setState(2); }
+                    else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){ 
+                        setState(4); 
+                    }
+                    else{ setState(-1); }
+                    break;
+            
+                case 1:
+                    if (ch >= '0' && ch <= '9'){
+                        setState(0); 
+                    }
+                    else if ((ch >= 'A' && ch <= 'K') || (ch >= 'a' && ch <= 'k')){
+                        setState(3); 
+                    }
+                    else if ((ch >= 'L' && ch <= 'Z') || (ch >= 'l' && ch <= 'z')){
+                        setState(4); 
+                    }
+                    else{ setState(-1); }
+                    break;
+
+                case 2:
+                    if (ch >= '0' && ch <= '9'){
+                        setState(0); 
+                    }
+                    else if ((ch >= 'A' && ch <= 'K') || (ch >= 'a' && ch <= 'k')){
+                        setState(4); 
+                    }
+                    else if ((ch >= 'L' && ch <= 'Z') || (ch >= 'l' && ch <= 'z')){
+                        setState(3); 
+                    }
+                    else{ setState(-1); }
+                    break;
+
+                case 3:
+                    if (ch >= '0' && ch <= '9'){
+                        setState(4); 
+                    }
+                    else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+                        setState(3);
+                    }
+                    else{ setState(-1); }
+                    break;
+
+                case 4:
+                    if ((ch >= '0' && ch <= '9') ||
+                        (ch >= 'a' && ch <= 'z') || 
+                        (ch >= 'A' && ch <= 'Z')){
+                        setState(4); 
+                    }
+                    else{ setState(-1); }
+                    break;
+            }
+        }
+
+        return getState() == 3;
+    }
+
+} 
--- lab/L1 (DFA implementation)/Test.java
+++ lab/L1 (DFA implementation)/Test.java
@@ -23,6 +23,16 @@ public class Test {
         System.out.println("Recognising \"" + str +"\" with Es1_2 automata: " + automata.test(str));
         System.out.println("");
 
+        //Es1_3
+        automata = new Es1_3();
+        str = "123456Bianchi";
+        System.out.println("**Es1_3 automata: recognises students of T2 and T3 lab turn**");
+        System.out.println("-- The string must structured with the id number followed by the student surname.");
+        System.out.println("-- T2: even id number, surname initial between A and K");
+        System.out.println("-- T3: odd id number, surname initial between L and Z");
+        System.out.println("Recognising \"" + str +"\" with Es1_3 automata: " + automata.test(str));
+        System.out.println("");
+
     }
     
 }
